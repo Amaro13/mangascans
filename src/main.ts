4dffa43eb65fa8b1f23c6b984d19878ce131bd22
Rikuster13 <amaro_ribeir0@hotmail.com>
2022-06-26 23:41:25 -0300

    App & Main using validation and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder() //this creates a documentBuilder for validation with swagger
+    .setTitle('MangaScans')
+    .setDescription('App for getting your best reading online')
+    .setVersion('1.0.0')
+    .addTag('status')
+    .addTag('users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3333);
 }
 bootstrap();
