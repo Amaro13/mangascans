5223ec1615b6a25cda21792e73fa07805b3487ed
Rikuster13 <amaro_ribeir0@hotmail.com>
2022-07-17 19:39:12 -0300

    feat(auth) added authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,17 @@ import { MangaModule } from './manga/manga.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { GenreModule } from './genre/genre.module';
 import { FavoritesModule } from './favorites/favorites.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, MangaModule, PrismaModule, GenreModule, FavoritesModule],
+  imports: [
+    UserModule,
+    MangaModule,
+    PrismaModule,
+    GenreModule,
+    FavoritesModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
