7554b9a4a05bf361a7af22acf9cbe0135c859de0
Rikuster13 <amaro_ribeir0@hotmail.com>
2022-07-17 19:37:02 -0300

    feat(auth): added authenticator

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { User } from 'src/users/entities/user.entity';
+import { AuthService } from './auth.service';
+import { LoginResponseDto } from './dto/login-response.dto';
+import { LoginDto } from './dto/login.dto';
+import { LoggedUser } from './logged-user.decorator';
+
+@Controller('auth')
+@ApiTags('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    summary: 'Make a login receiving a token authentication',
+  })
+  login(@Body() loginDto: LoginDto): Promise<LoginResponseDto> {
+    return this.authService.login(loginDto);
+  }
+
+  @Get()
+  @UseGuards(AuthGuard()) 
+  @ApiOperation({
+    summary: 'Returns the authenticated user in this moment',
+  })
+  @ApiBearerAuth()
+  profile(@LoggedUser() user: User) {
+    return user;
+  }
+}
